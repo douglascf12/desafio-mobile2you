01137424a68cbb788bc3d92845ce4428464ae78c
Douglas Cardoso <douglascf12@live.com>
2021-04-01 22:39:28 -0300

    requisição feita e enviado dados para a tela de exibição das infos do filme

--- DesafioMobile2You/ViewController.swift
+++ DesafioMobile2You/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var movie: Movie!
+    private var similarMovies: SimilarMovies!
+    private let id = 550
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadMovie()
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (_) in
+            if let vc = UIStoryboard(name: "Movie", bundle: nil).instantiateViewController(withIdentifier: "Movie") as? MovieTableViewController {
+                vc.movie = self.movie
+                vc.similarMovies = self.similarMovies
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! MovieTableViewController
+        vc.movie = movie
     }
 
-
+    func loadMovie() {
+        TheMovieDBAPI.getMovie(self.id) { (res) in
+            self.movie = res
+        } onError: { (error) in
+            print(error)
+        }
+        TheMovieDBAPI.getSimilarMovies(self.id) { (res) in
+            self.similarMovies = res
+        } onError: { (error) in
+            print(error)
+        }
+    }
+    
 }
-
