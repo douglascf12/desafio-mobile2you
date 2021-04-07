4e452a5f9751b4c6c0d0f37a3e79c2e3e71fddf9
Douglas Cardoso <douglascf12@live.com>
2021-04-07 13:46:33 -0300

    add reference ao storyboard via código

---
+++ DesafioMobile2You/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  DesafioMobile2You
+//
+//  Created by Douglas Cardoso Ferreira on 07/04/21.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let viewController = ViewController.instantiate()
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+    }
+}
--- DesafioMobile2You/View/ViewController.swift
+++ DesafioMobile2You/View/ViewController.swift
@@ -9,39 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Properties
-    private let id = 550
-    let movieManager = MovieManager.shared
+    static func instantiate() -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        return viewController
+    }
     
-    // MARK: - IBOutlets
-    @IBOutlet weak var aiLoading: UIActivityIndicatorView!
-    @IBOutlet weak var label: UILabel!
+//    // MARK: - Properties
+//    private let id = 550
+//    let movieManager = MovieManager.shared
+//
+//    // MARK: - IBOutlets
+//    @IBOutlet weak var aiLoading: UIActivityIndicatorView!
+//    @IBOutlet weak var label: UILabel!
 
     // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        aiLoading.startAnimating()
-        loadMovie()
-                
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (_) in
-            if let vc = UIStoryboard(name: "Movie", bundle: nil).instantiateViewController(withIdentifier: "Movie") as? MovieTableViewController {
-                self.present(vc, animated: true, completion: nil)
-            }
-        }
+        
+        
+        
+        
+//        aiLoading.startAnimating()
+//        loadMovie()
+//
+//        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (_) in
+//            if let vc = UIStoryboard(name: "Movie", bundle: nil).instantiateViewController(withIdentifier: "Movie") as? MovieTableViewController {
+//                self.present(vc, animated: true, completion: nil)
+//            }
+//        }
         
     }
     
-    func loadMovie() {
-        TheMovieDBAPI.getMovie(self.id) { (res) in
-            self.movieManager.movie = res
-        } onError: { (error) in
-            print(error)
-        }
-        TheMovieDBAPI.getSimilarMovies(self.id) { (res) in
-            self.movieManager.similarMovies = res
-        } onError: { (error) in
-            print(error)
-        }
-    }
+//    func loadMovie() {
+//        TheMovieDBAPI.getMovie(self.id) { (res) in
+//            self.movieManager.movie = res
+//        } onError: { (error) in
+//            print(error)
+//        }
+//        TheMovieDBAPI.getSimilarMovies(self.id) { (res) in
+//            self.movieManager.similarMovies = res
+//        } onError: { (error) in
+//            print(error)
+//        }
+//    }
     
 }
