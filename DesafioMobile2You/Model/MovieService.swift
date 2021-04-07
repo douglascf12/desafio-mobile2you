61b4ffd6e7fa7bb3f72eed550475a71d5fa2d3bb
Douglas Cardoso <douglascf12@live.com>
2021-04-07 13:54:13 -0300

    class MovieService criada

---
+++ DesafioMobile2You/Model/MovieService.swift
@@ -0,0 +1,13 @@
+//
+//  MovieService.swift
+//  DesafioMobile2You
+//
+//  Created by Douglas Cardoso Ferreira on 07/04/21.
+//
+
+import Foundation
+import RxSwift
+
+class MovieService {
+    
+}
